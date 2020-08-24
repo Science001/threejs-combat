536e983868161b41e7d386c849fe9c547c2cc3ac
Science001 <science.ganesh007@gmail.com>
2020-08-24 20:52:22 +0530

    Add support for Touch and Keyboard

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ A basic threejs app with two characters fighting. Created to get started with th
 
 The initial commit only had two characters who punch on click. The "Add Game Logic" commit will bring in the basic game features that enables the user to engage in an hand-to-hand combat with a monster. A fairly simple game.
 
-As of this commit, the Dodge feature does NOT work on Chrome Android as dblclick event is not monitorable there. Please use a PC or other mobile browsers to play the game. This should be fixed in future commits.
+~~As of this commit, the Dodge feature does NOT work on Chrome Android as dblclick event is not monitorable there. Please use a PC or other mobile browsers to play the game. This should be fixed in future commits.~~
+
+The app now works on mobile browsers as touch support has been added. PC users can also now play with keyboard.
+
+This game also has a fun easter egg. If you can find it, hit me up on Twitter at [@grktheXplorer](https://twitter.com/grktheXplorer).
--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import * as t from './three/build/three.module.js';
-import { toRad, loadModel, heroLives, monsterLives, updateLives, gameStart } from './helper.js';
+import { toRad, loadModel, heroLives, monsterLives, updateLives, gameStart, gameOver, setGameOver } from './helper.js';
 
 async function main() {
 
-    // Canvas Container
-    let container = document.querySelector('#scene');
+    // DOM Elements
+    let containerDOM = document.querySelector('#scene');
+    let nextTimeDOM = document.getElementById("nextTime");
+    let messageDOM = document.getElementById("message");
+    let livesBoxDOM = document.getElementById("livesBox");
+
 
     // Renderer
     let renderer = new t.WebGLRenderer({ antialias: true, alpha: true, });
-    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setSize(containerDOM.clientWidth, containerDOM.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
-    container.appendChild(renderer.domElement);
+    containerDOM.appendChild(renderer.domElement);
 
     // Scene and meta
     let scene = new t.Scene();
@@ -22,10 +26,11 @@ async function main() {
     const rightPlayerPosition = new t.Vector3(distanceBw, 0, 0.7);
     const rightPlayerRotation = new t.Vector3(0, toRad(-90 - faceOffRotation), 0);
     let heroIsOnRight = false;
+    let pattern = "";
 
     // Camera
     const fov = 45;
-    const aspect = container.clientWidth / container.clientHeight;
+    const aspect = containerDOM.clientWidth / containerDOM.clientHeight;
     const near = 0.1;
     const far = 500;
     let camera = new t.PerspectiveCamera(fov, aspect, near, far);
@@ -62,10 +67,12 @@ async function main() {
         idle: t.AnimationClip.findByName(allMonsterClips, 'idle'),
         punch: t.AnimationClip.findByName(allMonsterClips, 'punch'),
         dying: t.AnimationClip.findByName(allMonsterClips, 'dying'),
+        dance: t.AnimationClip.findByName(allMonsterClips, 'dance'),
     };
     const monsterIdleAnim = monsterMixer.clipAction(monsterClips.idle);
     const monsterPunchAnim = monsterMixer.clipAction(monsterClips.punch);
     const monsterDyingAnim = monsterMixer.clipAction(monsterClips.dying);
+    const monsterDanceAnim = monsterMixer.clipAction(monsterClips.dance);
     monsterPunchAnim.loop = t.LoopOnce;
     monsterDyingAnim.loop = t.LoopOnce;
     monsterIdleAnim.play();
@@ -83,15 +90,21 @@ async function main() {
                     heroReact();
                     if (!heroLives) {
                         heroDie();
+                        setGameOver();
+                        setTimeout(() => monsterDance(), 1200);
                     }
                 }
                 else if (heroPunchAnim.isRunning()) {
                     updateLives(heroLives - 1, monsterLives - 1, "Both Hurt");
                     if (!monsterLives) {
                         monsterDie();
+                        setGameOver();
+                        setTimeout(() => heroDance(), 1200);
                     }
                     if (!heroLives) {
                         heroDie();
+                        setGameOver();
+                        setTimeout(() => monsterDance(), 1200);
                     }
                 }
                 else updateLives(heroLives, monsterLives, "Dodged");
@@ -104,6 +117,20 @@ async function main() {
         }
     }
 
+    let nextTimeInterval;
+    let monsterPunchTimer = setInterval(() => {
+        if (gameStart) {
+            if (nextTimeInterval) clearInterval(nextTimeInterval);
+            monsterPunch();
+            let nextTime = 3;
+            nextTimeInterval = setInterval(() => {
+                if (nextTime > 0)
+                    nextTimeDOM.innerHTML = `Monster will attack in ${nextTime} ${nextTime === 1 ? "second" : "seconds"}`;
+                nextTime -= 1;
+            }, 1000);
+        }
+    }, 3000);
+
     function monsterDie() {
         const fadeInTime = 0.5;
         monsterDyingAnim.reset().play();
@@ -114,6 +141,18 @@ async function main() {
         else {
             monsterIdleAnim.stop();
             monsterPunchAnim.crossFadeTo(monsterDyingAnim, fadeInTime, true);
+        }
+    }
+
+    function monsterDance() {
+        const fadeInTime = 0.5;
+        monsterDanceAnim.reset().play();
+        if (monsterIdle) {
+            monsterIdleAnim.crossFadeTo(monsterDanceAnim, fadeInTime, true);
+        }
+        else {
+            monsterIdleAnim.stop();
+            monsterPunchAnim.crossFadeTo(monsterDanceAnim, fadeInTime, true);
         }
     }
 
@@ -142,42 +181,69 @@ async function main() {
         dodge: t.AnimationClip.findByName(allHeroClips, 'dodge'),
         react: t.AnimationClip.findByName(allHeroClips, 'react'),
         dying: t.AnimationClip.findByName(allHeroClips, 'dying'),
+        dance: t.AnimationClip.findByName(allHeroClips, 'dance'),
     };
     const heroIdleAnim = heroMixer.clipAction(heroClips.idle);
     const heroPunchAnim = heroMixer.clipAction(heroClips.punch);
     const heroDodgeAnim = heroMixer.clipAction(heroClips.dodge);
     const heroReactAnim = heroMixer.clipAction(heroClips.react);
     const heroDyingAnim = heroMixer.clipAction(heroClips.dying);
+    const heroDancingAnim = heroMixer.clipAction(heroClips.dance);
     heroPunchAnim.loop = t.LoopOnce;
     heroDodgeAnim.loop = t.LoopOnce;
     heroReactAnim.loop = t.LoopOnce;
     heroDyingAnim.loop = t.LoopOnce;
     heroIdleAnim.play();
 
-    function heroPunch() {
+    function heroPunch(key = "") {
         if (heroIdle) {
-            heroIdle = false;
-            const fadeInTime = 0.25;
-            const fadeOutTime = 0.15;
-            heroPunchAnim.reset().play();
-            heroIdleAnim.crossFadeTo(heroPunchAnim, fadeInTime, true);
-            setTimeout(() => {
-                if (monsterIdle) {
-                    updateLives(heroLives, monsterLives - 1, "Monster Hurt");
+            if (pattern === "UUDDLRL" && key === "R") {
+                pattern = "";
+                konami();
+            }
+            else {
+                heroIdle = false;
+                const fadeInTime = 0.25;
+                const fadeOutTime = 0.15;
+                heroPunchAnim.reset().play();
+                heroIdleAnim.crossFadeTo(heroPunchAnim, fadeInTime, true);
+                setTimeout(() => {
+                    if (monsterIdle) {
+                        updateLives(heroLives, monsterLives - 1, "Monster Hurt");
+                    }
+                    if (!monsterLives) {
+                        monsterDie();
+                        setGameOver();
+                        setTimeout(() => heroDance(), 1200);
+                    }
+                }, heroClips.punch.duration * 1000 / 2);
+                setTimeout(() => {
+                    heroIdleAnim.enabled = true;
+                    heroPunchAnim.crossFadeTo(heroIdleAnim, fadeOutTime, true);
+                    heroIdle = true;
+                }, heroClips.punch.duration * 1000);
+            }
+
+            // Konami
+            if (key === "U") {
+                if (pattern === "" || pattern === "U") pattern += "U";
+                else pattern = "";
+                if (pattern) console.log(pattern);
+            }
+            else if (key === "R") {
+                if (pattern === "UUDDL") pattern += "R";
+                else if (pattern === "UUDDLRL") {
+                    // Will never reach here, but stil...
+                    pattern = "";
+                    konami();
                 }
-                if (!monsterLives) {
-                    monsterDie();
-                }
-            }, heroClips.punch.duration * 1000 / 2);
-            setTimeout(() => {
-                heroIdleAnim.enabled = true;
-                heroPunchAnim.crossFadeTo(heroIdleAnim, fadeOutTime, true);
-                heroIdle = true;
-            }, heroClips.punch.duration * 1000);
+                else pattern = "";
+                if (pattern) console.log(pattern);
+            }
         }
     }
 
-    function heroDodge() {
+    function heroDodge(key = "") {
         if (heroIdle) {
             heroIdle = false;
             const fadeInTime = 0.5;
@@ -189,6 +255,18 @@ async function main() {
                 heroDodgeAnim.crossFadeTo(heroIdleAnim, fadeOutTime, true);
                 heroIdle = true;
             }, heroClips.dodge.duration * 1000);
+
+            // Konami
+            if (key === "D") {
+                if (pattern === "UU" || pattern === "UUD") pattern += "D";
+                else pattern = "";
+                if (pattern) console.log(pattern);
+            }
+            else if (key === "L") {
+                if (pattern === "UUDD" || pattern === "UUDDLR") pattern += "L";
+                else pattern = "";
+                if (pattern) console.log(pattern);
+            }
         }
     }
 
@@ -220,6 +298,18 @@ async function main() {
         }
     }
 
+    function heroDance() {
+        const fadeInTime = 0.5;
+        heroDancingAnim.reset().play();
+        if (heroIdle) {
+            heroIdleAnim.crossFadeTo(heroDancingAnim, fadeInTime, true);
+        }
+        else {
+            heroIdleAnim.stop();
+            heroPunchAnim.crossFadeTo(heroDancingAnim, fadeInTime, true);
+        }
+    }
+
     // ------------------------------------------------------------------------------
     function animate() {
         requestAnimationFrame(animate);
@@ -231,10 +321,26 @@ async function main() {
         let monsterDownTimer;
         let heroDownTimer;
 
-        if (!heroLives || !monsterLives) {
-            container.removeEventListener('click', onMouseClick);
-            container.removeEventListener('dblclick', onDblMouseClick);
+        if (!heroLives || !monsterLives || gameOver) {
+            containerDOM.removeEventListener('click', onMouseClick);
+            containerDOM.removeEventListener('dblclick', onDblMouseClick);
+            containerDOM.removeEventListener('touchstart', onTouchStart);
+            containerDOM.removeEventListener('touchmove', onTouchMove);
+            document.removeEventListener('keydown', onKeyDown);
             clearInterval(monsterPunchTimer);
+            clearInterval(nextTimeInterval);
+            if (heroIsOnRight) {
+                if (hero.rotation.y < 0)
+                    hero.rotation.y += 0.01;
+                if (monster.rotation.y > 0)
+                    monster.rotation.y -= 0.01;
+            }
+            else {
+                if (hero.rotation.y > 0)
+                    hero.rotation.y -= 0.01;
+                if (monster.rotation.y < 0)
+                    monster.rotation.y += 0.01;
+            }
         }
         if (!monsterLives) {
             monsterDownTimer = setTimeout(() => {
@@ -257,30 +363,16 @@ async function main() {
     // Events
 
     window.addEventListener('resize', () => {
-        camera.aspect = container.clientWidth / container.clientHeight;
+        camera.aspect = containerDOM.clientWidth / containerDOM.clientHeight;
         camera.updateProjectionMatrix();
-        renderer.setSize(container.clientWidth, container.clientHeight);
+        renderer.setSize(containerDOM.clientWidth, containerDOM.clientHeight);
     });
-
-    let monsterPunchTimer = setInterval(() => {
-        let nextTimeInterval;
-        if (gameStart) {
-            if (nextTimeInterval) clearInterval(nextTimeInterval);
-            monsterPunch();
-            let nextTime = 3;
-            nextTimeInterval = setInterval(() => {
-                if (nextTime > 0)
-                    document.getElementById("nextTime").innerHTML = `Monster will attack in ${nextTime} ${nextTime === 1 ? "second" : "seconds"}`;
-                nextTime -= 1;
-            }, 1000);
-        }
-    }, 3000);
 
     let clickTimer;
     const clickDelay = 300;
     let clickPrevent = false;
 
-    container.addEventListener('click', onMouseClick);
+    containerDOM.addEventListener('click', onMouseClick);
     function onMouseClick() {
         clickTimer = setTimeout(() => {
             if (!clickPrevent) {
@@ -290,11 +382,93 @@ async function main() {
         }, clickDelay);
     }
 
-    container.addEventListener('dblclick', onDblMouseClick)
+    containerDOM.addEventListener('dblclick', onDblMouseClick)
     function onDblMouseClick() {
         clearTimeout(clickTimer);
         clickPrevent = true;
         heroDodge();
+    }
+
+    // Keyboard events
+    document.addEventListener('keydown', onKeyDown);
+    function onKeyDown(evt) {
+        if (!gameStart) return;
+        if (evt.keyCode === 38) {
+            // Up Key
+            heroPunch("U");
+        }
+        else if (evt.keyCode === 40) {
+            // Down Key
+            heroDodge("D");
+        }
+        else if (evt.keyCode === 37) {
+            // Left Key
+            heroDodge("L");
+        }
+        else if (evt.keyCode === 39) {
+            // Right Key
+            heroPunch("R");
+        }
+    }
+
+    // Touch Events
+
+    containerDOM.addEventListener('touchstart', onTouchStart, false);
+    containerDOM.addEventListener('touchmove', onTouchMove, false);
+
+    let xDown;
+    let yDown;
+
+    function onTouchStart(evt) {
+        const firstTouch = evt.touches[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    };
+
+    function onTouchMove(evt) {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {
+            if (xDiff > 0) {
+                // Left Swipe
+                heroDodge("L");
+            } else {
+                // Right Swipe
+                heroPunch("R");
+            }
+        } else {
+            if (yDiff > 0) {
+                // Up Swipe
+                heroPunch("U");
+            } else {
+                // Down Swipe
+                heroDodge("D");
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    };
+
+    // Konami
+    function konami() {
+        console.log("KONAMI!");
+        setGameOver();
+        setTimeout(() => {
+            monsterDance();
+            heroDance();
+        }, 800);
+        messageDOM.innerHTML = "Let there be peace!";
+        nextTimeDOM.innerHTML = "You have found an easter egg!";
+        livesBoxDOM.style.display = "none";
     }
 }
 
